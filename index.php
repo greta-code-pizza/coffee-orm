abfb10419655f972b249e5a53b5f0d225b04c0f5
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-03-04 05:27:38 +0100

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require('vendor/autoload.php');
+
+if($_SERVER['HTTP_HOST'] !=  "supercoffeeeeeeee.herokuapp.com") {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
+}
+
+$path = "mysql:host=".$_ENV['DB_HOST'].":".$_ENV['DB_PORT'].";dbname=".$_ENV['DB_NAME'].";charset=utf8";
+	
+$pdo = 
+  new PDO(
+    $path, 
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASSWORD']
+  );
+
+		
+$sqlQuery = 'SELECT name FROM Waiter';
+
+foreach ($pdo->query($sqlQuery) as $waiter) {
+  echo "<p>" . $waiter['name'] . "</p>";
+}
